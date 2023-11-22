420014f56e6a3c4c771f56def65dcf12e5ae9e9c
Océane Vignot <82438041+Incitis@users.noreply.github.com>
2023-11-22 17:03:58 +0100

    sauvegarde des modifications avant le rebase

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+// Importing necessary modules
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3500;
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(logger);
 
+app.use(express.json());
+
+app.use(cookieParser());
+
+// Serving static files from the 'public' directory
+app.use("/", express.static(path.join(__dirname, "public")));
+
+// Handling routes using the root file
 app.use("/", require("./routes/root"));
 
+// Handling 404 errors for undefined routes
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
@@ -17,5 +30,7 @@ app.all("*", (req, res) => {
     res.type("txt").send(" 404 Not Found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
